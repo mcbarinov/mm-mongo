09c587524a67719a6357ec358aa0de3aa2360c26
mcbarinov <mcbarinov@proton.me>
2025-02-15 18:25:24 +0300

    import mongo_query from __init__.py

--- src/mm_mongo/__init__.py
+++ src/mm_mongo/__init__.py
@@ -1,4 +1,5 @@
 from mm_mongo.collection import MongoCollection as MongoCollection
+from mm_mongo.collection import mongo_query as mongo_query
 from mm_mongo.connection import MongoConnection as MongoConnection
 from mm_mongo.model import MongoModel as MongoModel
 from mm_mongo.pydantic import register_object_id_schema
