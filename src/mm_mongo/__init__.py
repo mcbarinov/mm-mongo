4fcb4feb208dc98da3dba313d01034c972977af0
mcbarinov <mcbarinov@proton.me>
2025-02-15 18:31:58 +0300

    json

--- src/mm_mongo/__init__.py
+++ src/mm_mongo/__init__.py
@@ -1,6 +1,8 @@
 from mm_mongo.collection import MongoCollection as MongoCollection
 from mm_mongo.collection import mongo_query as mongo_query
 from mm_mongo.connection import MongoConnection as MongoConnection
+from mm_mongo.json_ import CustomJSONEncoder as CustomJSONEncoder
+from mm_mongo.json_ import json_dumps as json_dumps
 from mm_mongo.model import MongoModel as MongoModel
 from mm_mongo.pydantic import register_object_id_schema
 from mm_mongo.types_ import DatabaseAny as DatabaseAny
