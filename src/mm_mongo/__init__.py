246271e8e86d0c41a071b4dd7025b3ef748ba8e7
mcbarinov <mcbarinov@proton.me>
2025-02-15 18:13:17 +0300

    import DatabaseAny from __init__.py

--- src/mm_mongo/__init__.py
+++ src/mm_mongo/__init__.py
@@ -2,5 +2,6 @@ from mm_mongo.collection import MongoCollection as MongoCollection
 from mm_mongo.connection import MongoConnection as MongoConnection
 from mm_mongo.model import MongoModel as MongoModel
 from mm_mongo.pydantic import register_object_id_schema
+from mm_mongo.types_ import DatabaseAny as DatabaseAny
 
 register_object_id_schema()
