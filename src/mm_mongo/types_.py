44c3b8893fff562e21ad4eddcf7ed8748e3ce253
mcbarinov <mcbarinov@proton.me>
2025-03-05 19:17:05 +0300

    add json_encoder to MongoResults

--- src/mm_mongo/types_.py
+++ src/mm_mongo/types_.py
@@ -4,7 +4,7 @@ from collections.abc import Mapping
 from typing import Any
 
 from bson import ObjectId
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from pymongo.database import Database
 from pymongo.results import DeleteResult, InsertManyResult, InsertOneResult, UpdateResult
 
@@ -16,6 +16,7 @@ type DatabaseAny = Database[DocumentType]
 
 
 class MongoUpdateResult[ID: IdType](BaseModel):
+    model_config = ConfigDict(json_encoders={ObjectId: str})
     acknowledged: bool
     matched_count: int
     modified_count: int
@@ -32,6 +33,7 @@ class MongoUpdateResult[ID: IdType](BaseModel):
 
 
 class MongoInsertOneResult[ID: IdType](BaseModel):
+    model_config = ConfigDict(json_encoders={ObjectId: str})
     acknowledged: bool
     inserted_id: ID
 
@@ -41,6 +43,7 @@ class MongoInsertOneResult[ID: IdType](BaseModel):
 
 
 class MongoInsertManyResult[ID: IdType](BaseModel):
+    model_config = ConfigDict(json_encoders={ObjectId: str})
     acknowledged: bool
     inserted_ids: list[ID]
 
